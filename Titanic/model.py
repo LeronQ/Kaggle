36d424f75b818e6bb5b08f84f1e3c50c40d2c7fa
LeronQ <qqiangllv@163.com>
2020-04-25 14:02:30 +0800

    modify the dataset

--- Titanic/model.py
+++ Titanic/model.py
@@ -2,7 +2,7 @@ import pandas as pd #数据分析
 import numpy as np #科学计算
 from pandas import Series,DataFrame
 
-data_train = pd.read_csv("Train.csv")
+data_train = pd.read_csv("data/Train.csv")
 # data_train
 
 
